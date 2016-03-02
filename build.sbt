cfbd21701975861e85d02b18b0193da39c7979fb
dfg129 <dfg129@gmail.com>
2016-03-02 14:14:11 -0500

    add test for simple http message

--- build.sbt
+++ build.sbt
@@ -19,12 +19,14 @@ libraryDependencies ++= Seq(
       "com.typesafe.akka" %% "akka-actor" % akkaVersion,
       "com.typesafe.akka" %% "akka-http-core" % akkaVersion,
       "com.typesafe.akka" %% "akka-http-experimental" % akkaVersion,
-      "com.typesafe.akka" %% "akka-http-spray-json-experimental" % akkaVersion,      
+      "com.typesafe.akka" %% "akka-http-spray-json-experimental" % akkaVersion,
       "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
       "com.typesafe.slick" %% "slick" % "3.1.1",
       "org.postgresql"     %  "postgresql"    % "9.4-1201-jdbc41",
       "org.slf4j" % "slf4j-nop" % "1.6.4",
       "org.scalactic" %% "scalactic" % "2.2.6",
-      "org.scalatest" %% "scalatest" % "2.2.6" % "test",
-      "io.spray" %%  "spray-json" % "1.3.2"
+      "org.scalatest" %% "scalatest" % "2.2.6" % Test,
+      "io.spray" %%  "spray-json" % "1.3.2",
+      "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0",
+      "ch.qos.logback" % "logback-classic" % "1.1.2"
 )
--- src/main/scala/com/statusofquo/container/docker/util/ContainerInfoActor.scala
+++ src/main/scala/com/statusofquo/container/docker/util/ContainerInfoActor.scala
@@ -2,7 +2,7 @@ package com.statusofquo.container
 package docker.util
 
 
-import akka.actor.{Actor, ActorSystem}
+import akka.actor.{Actor, ActorSystem, ActorRef, Props}
 import akka.event.Logging
 import akka.util.ByteString
 import akka.http.scaladsl.Http
@@ -10,29 +10,39 @@ import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.stream.ActorMaterializerSettings
 
-class ContainerInfo extends Actor {
+class ContainerInfoActor(next: ActorRef) extends Actor {
 
   import akka.pattern.pipe
   import context.dispatcher
 
-  val log = Logging(context.system, this)
   val actorSystem = ActorSystem("docker")
+
+  val log = Logging(actorSystem, this)
+
 
   final implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(actorSystem))
 
   val http = Http(actorSystem)
 
-  override def preStart() = {
-    log.info("In the preStart")
+ override def preStart() = {
+    log.debug("#######  In the preStart")
+
     http.singleRequest(HttpRequest(uri = "http://akka.io"))
-      .pipeTo(self)
-  }
+     .pipeTo(self)
+   }
 
   def receive = {
     case HttpResponse(StatusCodes.OK, headers, entity, _) =>
-      log.info("### Got response, body: " + entity.dataBytes.runFold(ByteString(""))(_ ++ _))
+      log.debug("### Got response, body: " + entity.dataBytes.runFold(ByteString(""))(_ ++ _))
     case HttpResponse(code, _, _, _) =>
-      log.info("### Request failed, response code: " + code)
-    case _ => println("############  inside ")
-  }
+      log.debug("### Request failed, response code: " + code)
+    case msg => {
+        log.debug("############  inside " + msg + " #### " + next)
+        next ! msg
+      }
+    }
+}
+
+object ContainerInfoActor {
+  def props(next: ActorRef): Props = Props(new ContainerInfoActor(next))
 }
